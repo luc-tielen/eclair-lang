d1ed1a9f1bab4e91bddeb5972406f0f0536aed05
Luc Tielen <luc.tielen@gmail.com>
2021-06-17 18:22:25 +0200

    Add parser for a small datalog subset

---
+++ lib/Eclair.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE OverloadedStrings, OverloadedLists #-}
+
+module Eclair ( AST(..), parseFile ) where
+
+import Data.Char
+import Data.Text (Text)
+import Data.Vector as V
+import Data.Void
+import Protolude
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Read as TR
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type ParseErr = Void
+type ParseError = P.ParseErrorBundle Text ParseErr
+type Parser = P.Parsec ParseErr Text
+
+
+type Number = Int
+
+newtype Id = Id Text
+  deriving (Eq, Show)
+
+data AST
+  = Lit Number
+  | Var Id
+  | Atom Id [AST]
+  | Rule Id [AST] [AST]
+  | Module [AST]
+  deriving (Eq, Show)
+
+
+parseFile :: FilePath -> IO (Either ParseError AST)
+parseFile path = do
+  contents <- TIO.readFile path
+  pure $ P.runParser astParser path contents
+
+astParser :: Parser AST
+astParser = do
+  whitespace
+  rules <- declParser `P.endBy` whitespace
+  P.eof
+  pure $ Module rules
+
+data DeclType = AtomType | RuleType
+
+declParser :: Parser AST
+declParser = do
+  name <- lexeme identifier
+  args <- lexeme $ betweenParens $ valueParser `P.sepBy1` comma
+  declType <- lexeme $ (RuleType <$ P.chunk ":-") <|> (AtomType <$ P.chunk ".")
+  case declType of
+    RuleType -> do
+      body <- atomParser `P.sepBy1` comma <* period
+      pure $ Rule name args body
+    AtomType -> pure $ Atom name args
+  where period = lexeme $ P.char '.'
+
+comma :: Parser Char
+comma = lexeme $ P.char ','
+
+atomParser :: Parser AST
+atomParser = do
+  name <- lexeme identifier
+  args <- lexeme $ betweenParens $ valueParser `P.sepBy1` comma
+  pure $ Atom name args
+valueParser :: Parser AST
+valueParser =  Var <$> identifier
+           <|> Lit <$> number
+
+identifier :: Parser Id
+identifier = Id <$> do
+  first <- P.letterChar P.<?> "start of identifier"
+  rest <- P.takeWhileP (Just "rest of identifier") isIdentifierChar
+  let parsed = T.cons first rest
+  when (parsed `V.elem` reserved) $ do
+    fail . T.unpack $ "Reserved keyword: " <> parsed
+  pure parsed
+  where isIdentifierChar c = isAlphaNum c || c == '_'
+        reserved = []
+
+number :: Parser Number
+number = do
+  firstDigit <- P.satisfy (`V.elem` ['1'..'9']) P.<?> "non-zero digit"
+  digits <- P.takeWhileP Nothing isDigit
+  P.notFollowedBy P.letterChar
+  case TR.decimal $ T.cons firstDigit digits of
+    Right (result, _) -> pure result
+    Left err -> panic . T.pack $ "Error occurred during parsing of decimal number: " <> err
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme whitespace
+
+whitespace :: Parser ()
+whitespace = L.space spaceParser commentParser blockCommentParser where
+  spaceParser = P.skipSome wsChar
+  wsChar = void (P.satisfy $ \c -> c == ' ' || c == '\n') P.<?> "whitespace"
+  commentParser = L.skipLineComment "//"
+  blockCommentParser = L.skipBlockComment "/*" "*/"
+
+betweenParens :: Parser a -> Parser a
+betweenParens =
+  P.between (lexeme $ P.char '(') (P.char ')') . lexeme
+
