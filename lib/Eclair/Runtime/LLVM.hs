6ce3a6a09408b206732e35210307e4d03283c774
Luc Tielen <luc.tielen@gmail.com>
2021-09-26 18:18:28 +0200

    Introduce Path concept for higher level handling of geps in LLVM

--- lib/Eclair/Runtime/LLVM.hs
+++ lib/Eclair/Runtime/LLVM.hs
@@ -1,12 +1,15 @@
-{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE RecursiveDo, PolyKinds #-}
 
 module Eclair.Runtime.LLVM
   ( module Eclair.Runtime.LLVM
   ) where
 
-import Protolude hiding ( Type )
+import Protolude hiding ( Type, (.) )
+import Control.Category
+import qualified Data.List.NonEmpty as NE
 import LLVM.IRBuilder.Module
 import LLVM.IRBuilder.Monad
+import LLVM.IRBuilder.Constant
 import LLVM.IRBuilder.Instruction
 import qualified LLVM.AST.IntegerPredicate as IP
 import qualified LLVM.AST.Constant as Constant
@@ -77,3 +80,36 @@ forLoop beginValue condition post asm = mdo
   br begin
   end <- block `named` "for_end"
   pure ()
+
+newtype Path (a :: k) (b :: k)
+  = Path (NonEmpty Operand)
+
+instance Category Path where
+  id = Path (pure $ int32 0)
+  Path b2c . Path a2b =
+    Path $ NE.head a2b :| (NE.tail a2b ++ NE.tail b2c)
+
+mkPath :: [Operand] -> Path a b
+mkPath path = Path (int32 0 :| path)
+
+pathToIndices :: Path a b -> [Operand]
+pathToIndices (Path indices) =
+  NE.toList indices
+
+addr :: Path a b -> Operand -> IRCodegen r Operand
+addr path p = gep p (pathToIndices path)
+
+deref :: Path a b -> Operand -> IRCodegen r Operand
+deref path p = do
+  addr <- addr path p
+  load addr 0
+
+assign :: Path a b -> Operand -> Operand -> IRCodegen r ()
+assign path p value = do
+  dstAddr <- addr path p
+  store dstAddr 0 value
+
+copy :: Path a b -> Operand -> Operand -> IRCodegen r ()
+copy path src dst = do
+  value <- deref path src
+  assign path dst value
