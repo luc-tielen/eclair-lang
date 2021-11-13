6280499554e44a50bb679d24c30d82feb8ecb5d0
Luc Tielen <luc.tielen@gmail.com>
2021-11-13 11:37:24 +0100

    Add Hash datatype for creating unique functions
    This is needed for example when generating instructions for 2 btrees (or
    another data structure), with different configuration (e.g. different
    index).

---
+++ lib/Eclair/Runtime/Hash.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE UndecidableInstances, TypeOperators, DefaultSignatures #-}
+
+module Eclair.Runtime.Hash
+  ( Hash
+  , unHash
+  , HashEnum(..)
+  , HashWithPrefix(..)
+  , ToHash(..)
+  ) where
+
+import Protolude
+import Prelude (String)
+import qualified Data.Text as T
+
+
+newtype Hash = Hash { unHash :: T.Text }
+
+instance Semigroup Hash where
+  Hash h1 <> Hash h2 =
+    Hash $ h1 <> "__" <> h2
+
+newtype HashEnum a = HashEnum a
+
+newtype HashWithPrefix (prefix :: Symbol) a
+  = HashWithPrefix a
+
+
+class ToHash a where
+  getHash :: a -> Hash
+  default getHash :: (Generic a, GToHash (Rep a)) => a -> Hash
+  getHash a = gGetHash (from a)
+
+instance ToHash T.Text where
+  getHash = Hash
+
+instance ToHash String where
+  getHash = Hash . T.pack
+
+instance ToHash Int where
+  getHash = Hash . T.pack . show
+
+instance ToHash a => ToHash [a] where
+  getHash = getFoldableHash
+
+instance ToHash a => ToHash (NonEmpty a) where
+  getHash = getFoldableHash
+
+instance ToHash a => ToHash (Set a) where
+  getHash = getFoldableHash
+
+getFoldableHash :: (Foldable f, ToHash a) => f a -> Hash
+getFoldableHash =
+  Hash . mconcat . intersperse "_" . map (unHash . getHash) . toList
+
+instance (Enum a) => ToHash (HashEnum a) where
+  getHash (HashEnum a) = getHash $ fromEnum a
+
+instance forall prefix a. (KnownSymbol prefix, Generic a, GToHash (Rep a))
+  => ToHash (HashWithPrefix prefix a) where
+  getHash x@(HashWithPrefix a) =
+    let pre = Hash $ T.pack $ symbolVal (Proxy :: Proxy prefix)
+        h = gGetHash (from a)
+     in pre <> h
+
+
+class GToHash f where
+  gGetHash :: f a -> Hash
+
+instance ToHash a => GToHash (K1 i a) where
+  gGetHash (K1 x) = getHash x
+
+instance GToHash a => GToHash (M1 i c a) where
+  gGetHash (M1 x) = gGetHash x
+
+instance (GToHash f, GToHash g) => GToHash (f :*: g) where
+  gGetHash (a :*: b) =
+    gGetHash a <> gGetHash b
+
