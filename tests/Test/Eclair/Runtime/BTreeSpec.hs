fe759041affe2ebf6886e426461757989c2957a9
Luc Tielen <luc.tielen@gmail.com>
2022-01-03 12:25:46 +0100

    Add unit test for setup and teardown of empty btree

--- tests/Test/Eclair/Runtime/BTreeSpec.hs
+++ tests/Test/Eclair/Runtime/BTreeSpec.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE MultiWayIf, TypeApplications #-}
 
 module Test.Eclair.Runtime.BTreeSpec
   ( module Test.Eclair.Runtime.BTreeSpec
   ) where
 
 import Protolude hiding (Meta)
+import Data.Coerce
 import Data.IORef
 import Data.Maybe (fromJust)
 import qualified Data.Text.Encoding as TE
-import Test.Hspec
+import Test.Hspec hiding (Arg)
 import Control.Monad.Cont
 import Data.ByteString.Short hiding (index)
 import Eclair.Runtime.BTree
@@ -26,49 +27,7 @@ import qualified LLVM.CodeModel as CodeModel
 import qualified LLVM.CodeGenOpt as CodeGenOpt
 import qualified LLVM.Internal.OrcJIT.CompileLayer as CL
 import Foreign
-
-foreign import ccall "dynamic" foreignInitEmpty
-  :: FunPtr (Ptr Tree -> IO ()) -> Ptr Tree -> IO ()
-foreign import ccall "dynamic" foreignDestroy
-  :: FunPtr (Ptr Tree -> IO ()) -> Ptr Tree -> IO ()
-foreign import ccall "dynamic" foreignPurge
-  :: FunPtr (Ptr Tree -> IO ()) -> Ptr Tree -> IO ()
-foreign import ccall "dynamic" foreignSwap
-  :: FunPtr (Ptr Tree -> Ptr Tree -> IO ())
-  -> Ptr Tree -> Ptr Tree -> IO ()
-foreign import ccall "dynamic" foreignBegin
-  :: FunPtr (Ptr Tree -> Ptr Iter -> IO ())
-  -> Ptr Tree -> Ptr Iter -> IO ()
-foreign import ccall "dynamic" foreignEnd
-  :: FunPtr (Ptr Tree -> Ptr Iter -> IO ())
-  -> Ptr Tree -> Ptr Iter -> IO ()
-foreign import ccall "dynamic" foreignInsert
-  :: FunPtr (Ptr Tree -> Ptr Value -> IO Bool)
-  -> Ptr Tree -> Ptr Value -> IO Bool
-foreign import ccall "dynamic" foreignInsertRange
-  :: FunPtr (Ptr Tree -> Ptr Iter -> Ptr Iter -> IO ())
-  -> Ptr Tree -> Ptr Iter -> Ptr Iter -> IO ()
-foreign import ccall "dynamic" foreignIsEmpty
-  :: FunPtr (Ptr Tree -> IO Bool)
-  -> Ptr Tree -> IO Bool
-foreign import ccall "dynamic" foreignLowerBound
-  :: FunPtr (Ptr Tree -> Ptr Value -> Ptr Iter -> IO ())
-  -> Ptr Tree -> Ptr Value -> Ptr Iter -> IO ()
-foreign import ccall "dynamic" foreignUpperBound
-  :: FunPtr (Ptr Tree -> Ptr Value -> Ptr Iter -> IO ())
-  -> Ptr Tree -> Ptr Value -> Ptr Iter -> IO ()
-foreign import ccall "dynamic" foreignContains
-  :: FunPtr (Ptr Tree -> Ptr Value -> IO Bool)
-  -> Ptr Tree -> Ptr Value -> IO Bool
-foreign import ccall "dynamic" foreignIterIsEqual
-  :: FunPtr (Ptr Iter -> Ptr Iter -> IO Bool)
-  -> Ptr Iter -> Ptr Iter -> IO Bool
-foreign import ccall "dynamic" foreignIterCurrent
-  :: FunPtr (Ptr Iter -> IO (Ptr Value))
-  -> Ptr Iter -> IO (Ptr Value)
-foreign import ccall "dynamic" foreignIterNext
-  :: FunPtr (Ptr Iter -> IO ())
-  -> Ptr Iter -> IO ()
+import Foreign.LibFFI
 
 
 jit :: ModuleBuilderT IO a -> (forall l. CompileLayer l => l -> a -> IO b) -> IO b
@@ -139,59 +98,90 @@ settings
   , searchType = Linear
   }
 
-jitCompile :: IO FFI
-jitCompile = jit (codegen settings) $ \compileLayer (_, sizes) -> do
-  fnInitEmpty <- importSymbol compileLayer "btree_init_empty" foreignInitEmpty
-  fnDestroy <- importSymbol compileLayer "btree_destroy" foreignDestroy
-  fnPurge <- importSymbol compileLayer "btree_clear" foreignPurge
-  fnSwap <- importSymbol compileLayer "btree_swap" foreignSwap
-  fnBegin <- importSymbol compileLayer "btree_begin" foreignBegin
-  fnEnd <- importSymbol compileLayer "btree_end" foreignEnd
-  fnInsert <- importSymbol compileLayer "btree_insert_value" foreignInsert
-  fnInsertRange <- importSymbol compileLayer "btree_insert_range" foreignInsertRange
-  fnIsEmpty <- importSymbol compileLayer "btree_is_empty" foreignIsEmpty
-  fnLowerBound <- importSymbol compileLayer "btree_lower_bound" foreignLowerBound
-  fnUpperBound <- importSymbol compileLayer "btree_upper_bound" foreignUpperBound
-  fnContains <- importSymbol compileLayer "btree_contains" foreignContains
-  fnIterIsEqual <- importSymbol compileLayer "iterator_is_equal" foreignIterIsEqual
-  fnIterCurrent <- importSymbol compileLayer "iterator_current" foreignIterCurrent
-  fnIterNext <- importSymbol compileLayer "iterator_next" foreignIterNext
-  pure FFI { ffiWithEmptyTree = withResource (treeSize sizes) fnInitEmpty fnDestroy
-           , ffiPurge = fnPurge
-           , ffiSwap = fnSwap
-           , ffiBegin = allocateAndApply (valueSize sizes) . fnBegin
-           , ffiEnd = allocateAndApply (valueSize sizes) . fnEnd
-           , ffiInsert = fnInsert
-           , ffiInsertRange = fnInsertRange
-           , ffiIsEmpty = fnIsEmpty
-           , ffiLowerBound = \tree val -> allocateAndApply (iterSize sizes) (fnLowerBound tree val)
-           , ffiUpperBound = \tree val -> allocateAndApply (iterSize sizes) (fnUpperBound tree val)
-           , ffiContains = fnContains
-           , ffiIterIsEqual = fnIterIsEqual
-           , ffiIterCurrent = fnIterCurrent
-           , ffiIterNext = fnIterNext
-           }
+jitCompile :: (FFI -> IO ()) -> IO ()
+jitCompile action = jit (codegen settings) $ \compileLayer (_, sizes) -> do
+  fnInitEmpty <- importSymbol compileLayer "btree_init_empty"
+  fnDestroy <- importSymbol compileLayer "btree_destroy"
+  fnPurge <- importSymbol compileLayer "btree_clear"
+  fnSwap <- importSymbol compileLayer "btree_swap"
+  fnBegin <- importSymbol @(Ptr Tree -> Ptr Iter -> IO ()) compileLayer "btree_begin"
+  fnEnd <- importSymbol @(Ptr Tree -> Ptr Iter -> IO ()) compileLayer "btree_end"
+  fnInsert <- importSymbol compileLayer "btree_insert_value"
+  fnInsertRange <- importSymbol compileLayer "btree_insert_range"
+  fnIsEmpty <- importSymbol compileLayer "btree_is_empty"
+  fnLowerBound <- importSymbol @(Ptr Tree -> Ptr Value -> Ptr Iter -> IO ()) compileLayer "btree_lower_bound"
+  fnUpperBound <- importSymbol @(Ptr Tree -> Ptr Value -> Ptr Iter -> IO ()) compileLayer "btree_upper_bound"
+  fnContains <- importSymbol compileLayer "btree_contains"
+  fnIterIsEqual <- importSymbol compileLayer "iterator_is_equal"
+  fnIterCurrent <- importSymbol compileLayer "iterator_current"
+  fnIterNext <- importSymbol compileLayer "iterator_next"
+  let ffi = FFI { ffiWithEmptyTree = withResource (treeSize sizes) fnInitEmpty fnDestroy
+                , ffiPurge = fnPurge
+                , ffiSwap = fnSwap
+                , ffiBegin = allocateAndApply (valueSize sizes) . fnBegin
+                , ffiEnd = allocateAndApply (valueSize sizes) . fnEnd
+                , ffiInsert = fnInsert
+                , ffiInsertRange = fnInsertRange
+                , ffiIsEmpty = fnIsEmpty
+                , ffiLowerBound = \tree val -> allocateAndApply (iterSize sizes) (fnLowerBound tree val)
+                , ffiUpperBound = \tree val -> allocateAndApply (iterSize sizes) (fnUpperBound tree val)
+                , ffiContains = fnContains
+                , ffiIterIsEqual = fnIterIsEqual
+                , ffiIterCurrent = fnIterCurrent
+                , ffiIterNext = fnIterNext
+                }
+  action ffi
 
-importSymbol :: CompileLayer l => l -> Text -> (FunPtr a -> a) -> IO a
-importSymbol compileLayer symbol fn = do
+importSymbol :: Import a => CompileLayer l => l -> Text -> IO a
+importSymbol compileLayer symbol = do
   let hash = getHash settings
       hashedSymbol = symbol <> "_" <> unHash hash
       hashedSymbol' = toShort $ TE.encodeUtf8 hashedSymbol
   mangled <- mangleSymbol compileLayer hashedSymbol'
   Right (JITSymbol symbolPtr _) <- CL.findSymbol compileLayer mangled True
-  pure $ fn $ castPtrToFunPtr $ wordPtrToPtr symbolPtr
+  wrap $ castPtrToFunPtr $ wordPtrToPtr symbolPtr
+
+class Import a where
+  wrap :: FunPtr a -> IO a
+
+instance Import (Ptr a -> IO ()) where
+  wrap fn = do
+    pure $ \ptr -> do
+      callFFI fn retVoid [argPtr ptr]
+
+instance Import (Ptr a -> IO Bool) where
+  wrap fn = do
+    pure $ \ptr -> (== 1) <$> callFFI fn retCInt [argPtr ptr]
+
+instance Import (Ptr a -> IO (Ptr Value)) where
+  wrap fn = do
+    pure $ \ptr -> callFFI fn (retPtr (panic "Unused arg")) [argPtr ptr]
+
+instance Import (Ptr a -> Ptr b -> IO ()) where
+  wrap fn = do
+    pure $ \ptr1 ptr2 -> callFFI fn retVoid [argPtr ptr1, argPtr ptr2]
+
+instance Import (Ptr a -> Ptr b -> IO Bool) where
+  wrap fn = do
+    pure $ \ptr1 ptr2 -> (== 1) <$> callFFI fn retCInt [argPtr ptr1, argPtr ptr2]
+
+instance Import (Ptr a -> Ptr b -> Ptr c -> IO ()) where
+  wrap fn = do
+    pure $ \ptr1 ptr2 ptr3 -> callFFI fn retVoid [argPtr ptr1, argPtr ptr2, argPtr ptr3]
+
+instance Import (Ptr a -> Ptr b -> Ptr c -> IO Bool) where
+  wrap fn = do
+    pure $ \ptr1 ptr2 ptr3 -> (== 1) <$> callFFI fn retCInt [argPtr ptr1, argPtr ptr2, argPtr ptr3]
+
 
 withResource :: Word64            -- number of bytes to allocate
              -> (Ptr a -> IO ())  -- "constructor"
              -> (Ptr a -> IO ())  -- "destructor"
              -> (Ptr a -> IO b)
              -> IO b
-withResource numBytes construct destruct =
-  bracket (mask_ $ do
-            ptr <- mallocBytes (fromIntegral numBytes)
-            construct ptr
-            pure ptr)
-          (\ptr -> destruct ptr *> free ptr)
+withResource numBytes construct destruct f = do
+  allocaBytes (fromIntegral numBytes) $ \ptr -> do
+    bracket (construct ptr *> pure ptr) destruct f
 
 allocateAndApply :: Word64 -> (Ptr a -> IO b) -> IO (ForeignPtr a)
 allocateAndApply size f = do
@@ -240,42 +230,48 @@ getAllValues ffi tree = do
 
 -- TODO: generate list of ops using hedgehog, run program with that
 
+runContT_ :: Monad m => ContT () m a -> m ()
+runContT_ = flip runContT (const $ pure ())
+
 spec :: Spec
-spec = describe "btree" $ beforeAll jitCompile $ parallel $ do
-  it "can create and destroy btrees" $ \ffi -> pending
+spec = describe "btree" $ parallel $ do
+  fit "can create and destroy btrees" $ runContT_ $ do
+      ffi <- ContT jitCompile
+      tree <- ContT $ ffiWithEmptyTree ffi
+      lift $ tree `shouldNotBe` nullPtr
 
-  it "can iterate over the full range of values" $ \ffi -> pending
+  it "can iterate over the full range of values" $ pending
 
-  it "can use lower- and upper-bound to iterate over a subset of values" $ \ffi -> pending
+  it "can use lower- and upper-bound to iterate over a subset of values" $ pending
 
   -- TODO: properties
-  it "should be empty after purging" $ \ffi -> do
+  it "should be empty after purging" $ do
     pending
 
   describe "swap" $ parallel $ do
-    it "swaps contents of tree A and B" $ \ffi -> pending
+    it "swaps contents of tree A and B" $ pending
 
-    it "is a no-op to swap twice" $ \ffi -> pending
+    it "is a no-op to swap twice" $ pending
 
   describe "insert" $ parallel $ do
-    it "is not empty afterwards" $ \ffi -> pending
+    it "is not empty afterwards" $ pending
 
-    it "does nothing if value is already stored in tree" $ \ffi -> pending
+    it "does nothing if value is already stored in tree" $ pending
 
-    it "adds the new value if not stored in tree" $ \ffi -> pending
+    it "adds the new value if not stored in tree" $ pending
 
-    it "is commutative" $ \ffi -> pending
+    it "is commutative" $ pending
 
   describe "insertRange" $ parallel $ do
-    it "increases in size by up to N when adding N elements" $ \ffi -> pending
+    it "increases in size by up to N when adding N elements" $ pending
     -- TODO same props as insert?
 
   describe "isEmpty" $ parallel $ do
-    it "returns true for empty trees" $ \ffi -> pending
+    it "returns true for empty trees" $ pending
 
-    it "returns false for non-empty trees" $ \ffi -> pending
+    it "returns false for non-empty trees" $ pending
 
   describe "contains" $ parallel $ do
-    it "returns true if element is inside the tree" $ \ffi -> pending
+    it "returns true if element is inside the tree" $ pending
 
-    it "returns false if element is not inside the tree" $ \ffi -> pending
+    it "returns false if element is not inside the tree" $ pending
