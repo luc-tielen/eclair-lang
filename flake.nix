a7c6a1039187324db24a6f7670bd80780bfcb456
Luc Tielen <luc.tielen@gmail.com>
2022-05-27 23:07:37 +0200

    Nix: integrate llvm-codegen library/flake

--- flake.nix
+++ flake.nix
@@ -7,21 +7,14 @@
     ds.url = "github:numtide/devshell?ref=master";
     hls.url = "github:haskell/haskell-language-server?ref=master";
     shs.url =
-      "github:luc-tielen/souffle-haskell?rev=7bfa29f52f2056459d160e795d2e7b8f36af801a";
-    llvm-hs.url =
-      "github:luc-tielen/llvm-hs?rev=69ae96c9eea8531c750c9d81f9813286ef5ced81";
-    llvm-hs.flake = false;
-    llvm-hs-pretty.url =
-      "github:luc-tielen/llvm-hs-pretty?rev=990bb6981f6214d9c1bbf46fd9e9ce5596d3bf30";
-    llvm-hs-pretty.flake = false;
-    llvm-hs-combinators.url =
-      "github:luc-tielen/llvm-hs-combinators?rev=6a5494d00d55dc2d988957588cf204731f27abc1";
-    llvm-hs-combinators.flake = false;
+      "github:luc-tielen/souffle-haskell?rev=c46d0677e4bc830df89ec1de2396c562eb9d86d3";
+    llvm-cg.url =
+      "github:luc-tielen/llvm-codegen?rev=cca111af3498895d09b65b6316fb0e2af5219163";
     alga.url =
       "github:snowleopard/alga?rev=75de41a4323ab9e58ca49dbd78b77f307b189795";
     alga.flake = false;
   };
-  outputs = { self, np, fu, ds, shs, ... }@inputs:
+  outputs = { self, np, fu, ds, shs, llvm-cg, ... }@inputs:
     with np.lib;
     with fu.lib;
     eachSystem [ "x86_64-linux" ] (system:
@@ -36,29 +29,9 @@
             haskellPackages =
               final.haskell.packages."ghc${ghcVersion}".override {
                 overrides = hf: hp:
-                  with final.haskell.lib; {
+                  with final.haskell.lib; rec {
                     inherit (shs.packages."${system}") souffle-haskell;
-
-                    llvm-config = final.llvmPackages_9.llvm;
-
-                    llvm-config-dev = final.llvmPackages_9.llvm.dev;
-
-                    llvm-hs-pure = with hf;
-                      (callCabal2nix "llvm-hs-pure"
-                        "${inputs.llvm-hs}/llvm-hs-pure" { });
-
-                    llvm-hs = with hf;
-                      dontHaddock
-                      (callCabal2nix "llvm-hs" "${inputs.llvm-hs}/llvm-hs" { });
-
-                    llvm-hs-pretty = with hf;
-                      dontCheck (doJailbreak
-                        (callCabal2nix "llvm-hs-pretty" (inputs.llvm-hs-pretty)
-                          { }));
-
-                    llvm-hs-combinators = with hf;
-                      callCabal2nix "llvm-hs-combinators"
-                      (inputs.llvm-hs-combinators) { };
+                    inherit (llvm-cg.packages."${system}") llvm-codegen;
 
                     algebraic-graphs = with hf;
                       dontCheck
@@ -78,7 +51,12 @@
 
         pkgs = import np {
           inherit system config;
-          overlays = [ overlay ds.overlay ] ++ shs.overlays."${system}";
+          overlays = [
+            overlay
+            ds.overlay
+            shs.overlay."${system}"
+            llvm-cg.overlay."${system}"
+          ];
         };
       in with pkgs.lib; rec {
         inherit overlay;
@@ -90,15 +68,11 @@
           packages = with pkgs;
             with haskellPackages; [
               pkgs.ghcid
-              pkgs.llvmPackages_9.llvm.dev
               (ghcWithPackages (p:
                 with p; [
                   algebraic-graphs
                   hspec-discover
-                  llvm-hs
-                  llvm-hs-pure
-                  llvm-hs-pretty
-                  llvm-hs-combinators
+                  llvm-codegen
                   souffle-haskell
                   ghc
                   cabal-install
