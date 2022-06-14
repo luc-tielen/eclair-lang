08c88fce2491b377345a8d2217b9367ec4396c6e
Luc Tielen <luc.tielen@gmail.com>
2022-06-14 09:10:23 +0200

    Hack to run semantic analysis before rest of compiler pipeline

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,18 @@ module Main (main) where
 import qualified Data.Text.Lazy.IO as T
 import Control.Exception
 import LLVM.Pretty
+import qualified Eclair.AST.Analysis as SA
 import Eclair.ArgParser
 import Eclair
 import GHC.IO.Encoding
 
+
+-- TODO: remove this hack, should be integrated into main compiler line.
+-- this is only needed atm since main compiler pipeline doesn't use the actual semantic analysis results yet.
+manualSemanticAnalysis :: FilePath -> IO ()
+manualSemanticAnalysis file = do
+  result <- semanticAnalysis file
+  liftIO $ forM_ (SA.maybeToSemanticError result) (throwIO . SemanticErr)
 
 main :: IO ()
 main = do
@@ -19,4 +27,6 @@ main = do
             EmitRA -> emitRA
             EmitEIR -> emitEIR
             EmitLLVM -> emitLLVM
-      fn file `catch` handleErrors
+      handle handleErrors $ do
+        manualSemanticAnalysis file
+        fn file
