5e8a68ff51b85c2a23d0cef744b4bf836f403892
Luc Tielen <luc.tielen@gmail.com>
2022-06-18 13:39:17 +0200

    Add transform to shift assignments to the end of a rule

--- lib/Eclair/AST/Transforms.hs
+++ lib/Eclair/AST/Transforms.hs
@@ -5,8 +5,10 @@ module Eclair.AST.Transforms
 import Eclair.AST.IR
 import Eclair.Transform
 import qualified Eclair.AST.Transforms.RemoveWildcards as RmWildcards
+import qualified Eclair.AST.Transforms.ShiftAssignments as ShiftAssigns
 
 
 simplify :: AST -> AST
 simplify =
-  runTransform RmWildcards.transform
+  runTransform $ RmWildcards.transform
+             >>> ShiftAssigns.transform
