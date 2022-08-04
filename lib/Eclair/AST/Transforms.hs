49324c28fbb3df71d877fde3634c683cbe3b23f0
Luc Tielen <luc.tielen@gmail.com>
2022-08-04 22:25:56 +0200

    Rename NormalizeVarEqualities -> UniqueVars

--- lib/Eclair/AST/Transforms.hs
+++ lib/Eclair/AST/Transforms.hs
@@ -6,11 +6,11 @@ import Eclair.AST.IR
 import Eclair.Transform
 import qualified Eclair.AST.Transforms.RemoveWildcards as RmWildcards
 import qualified Eclair.AST.Transforms.ShiftAssignments as ShiftAssigns
-import qualified Eclair.AST.Transforms.NormalizeVarEqualities as NormalizeVarEqs
+import qualified Eclair.AST.Transforms.UniqueVars as UniqueVars
 
 
 simplify :: NodeId -> AST -> AST
 simplify nodeId =
   runTransform nodeId $ RmWildcards.transform
-                    >>> NormalizeVarEqs.transform
+                    >>> UniqueVars.transform
                     >>> ShiftAssigns.transform
--- lib/Eclair/AST/Transforms/UniqueVars.hs
+++ lib/Eclair/AST/Transforms/UniqueVars.hs
@@ -1,4 +1,4 @@
-module Eclair.AST.Transforms.NormalizeVarEqualities
+module Eclair.AST.Transforms.UniqueVars
   ( transform
   ) where
 
