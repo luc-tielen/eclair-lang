a3193aeb7acbd52fec743cb377ea0449d8e7a340
Luc Tielen <luc.tielen@gmail.com>
2022-09-29 20:37:09 +0200

    Add LLVM codegen for symbols (Eclair strings)

---
+++ lib/Eclair/LLVM/Symbol.hs
@@ -0,0 +1,102 @@
+module Eclair.LLVM.Symbol
+  ( Symbol(..)
+  , codegen
+  ) where
+
+import Prelude hiding (Symbol, void)
+import LLVM.Codegen
+import Control.Monad.Morph
+import Eclair.LLVM.Runtime
+
+
+data Symbol
+  = Symbol
+  { tySymbol :: Type  -- A symbol (string). Assumes UTF-8 encoding.
+  , symbolInit :: Operand
+  , symbolDestroy :: Operand
+  , symbolIsEqual :: Operand
+  }
+
+data CGState
+  = CGState
+  { externals :: Externals
+  , tySym :: Type
+  }
+
+type ModuleCodegen = ReaderT CGState ModuleBuilder
+type IRCodegen = IRBuilderT ModuleCodegen
+
+codegen :: Externals -> ModuleBuilder Symbol
+codegen exts = do
+  symbolTy <- generateTypes
+  runReaderT generateFunctions $ CGState exts symbolTy
+
+generateTypes :: ModuleBuilder Type
+generateTypes =
+  -- For now, only up to 4GB of strings are supported.
+  typedef "symbol_t" Off [i32, ptr i8]
+
+generateFunctions :: ModuleCodegen Symbol
+generateFunctions = do
+  symbolTy <- asks tySym
+  symInit <- mkSymbolInit
+  symDestroy <- mkSymbolDestroy
+  symIsEqual <- mkSymbolIsEqual
+  pure $ Symbol
+    { tySymbol = symbolTy
+    , symbolInit = symInit
+    , symbolDestroy = symDestroy
+    , symbolIsEqual = symIsEqual
+    }
+
+-- NOTE: this copies data to the new struct, and assumes memory has already been
+-- allocated for the symbol. Don't pass a const char* / ptr i8 that point to
+-- static memory because it will be freed during cleanup.
+mkSymbolInit :: ModuleCodegen Operand
+mkSymbolInit = do
+  symbolTy <- asks tySym
+  let args = [(ptr symbolTy, "symbol"), (i32, "size"), (ptr i8, "data")]
+  function "symbol_init" args void $ \[symbol, size, utf8Data] -> do
+    -- assert(symbol && "symbol cannot be NULL!");
+    -- assert(utf8Data && "data cannot be NULL!");
+    assign sizeOf symbol size
+    assign dataOf symbol utf8Data
+
+-- NOTE: this only destroys the memory this symbol is pointing to.
+mkSymbolDestroy :: ModuleCodegen Operand
+mkSymbolDestroy = do
+  (symbolTy, freeFn) <- asks (tySym &&& extFree . externals)
+  let args = [(ptr symbolTy, "symbol")]
+  function "symbol_destroy" args void $ \[symbol] -> do
+    -- assert(symbol && "symbol cannot be NULL!");
+    dataPtr <- addr dataOf symbol
+    call freeFn [dataPtr]
+
+mkSymbolIsEqual :: ModuleCodegen Operand
+mkSymbolIsEqual = do
+  (symbolTy, memcmpFn) <- asks (tySym &&& extMemcmp . externals)
+  let args = [(ptr symbolTy, "symbol1"), (ptr symbolTy, "symbol2")]
+  function "symbol_is_equal" args i1 $ \[symbol1, symbol2] -> do
+    -- assert(symbol1 && "symbol1 cannot be NULL!");
+    -- assert(symbol2 && "symbol2 cannot be NULL!");
+    size1 <- deref sizeOf symbol1
+    size2 <- deref sizeOf symbol2
+    isNotEqualSize <- size1 `ne` size2
+    if' isNotEqualSize $
+      ret $ bit 0
+
+    data1 <- deref dataOf symbol1
+    data2 <- deref dataOf symbol2
+    isDataEqual <- (`eq` bit 0) =<< call memcmpFn [data1, data2, size1, bit 0]
+    ret isDataEqual
+
+data Index
+  = SymbolIdx
+  | SizeIdx
+  | DataIdx
+
+sizeOf :: Path 'SymbolIdx 'SizeIdx
+sizeOf = mkPath [int32 0]
+
+dataOf :: Path 'SymbolIdx 'DataIdx
+dataOf = mkPath [int32 1]
