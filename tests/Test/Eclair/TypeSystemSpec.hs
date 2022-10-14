a102c4a6fdb3645ee28b444c83381713c2b559e9
Luc Tielen <luc.tielen@gmail.com>
2022-10-14 07:39:03 +0200

    Convert more tests to use lit

--- tests/Test/Eclair/TypeSystemSpec.hs
+++ tests/Test/Eclair/TypeSystemSpec.hs
@@ -29,90 +29,7 @@ failsWith errs code =
 
 spec :: Spec
 spec = describe "typesystem" $ parallel $ do
-  -- NOTE: though this should give a dead code error somewhere else
-  it "typechecks if there are no rules found for a type declaration" $ do
-    typeChecks [text|
-      @def edge(u32, u32).
-      @def chain(u32, u32, u32).
-      |]
-
-  it "returns type errors for duplicate type declarations" $ do
-    typeChecks [text|
-      @def edge(u32, u32).
-      @def path(u32, u32).
-      |]
-    failsWith [DuplicateTypeDeclaration (Id "edge") (fromList [(NodeId 1, [U32, U32]), (NodeId 2, [U32, U32])])] [text|
-      @def edge(u32, u32).
-      @def edge(u32, u32).
-      |]
-    failsWith [DuplicateTypeDeclaration (Id "edge") (fromList [(NodeId 1, [U32, U32]), (NodeId 2, [U32]), (NodeId 3, [U32, U32, U32])])] [text|
-      @def edge(u32, u32).
-      @def edge(u32).
-      @def edge(u32, u32, u32).
-      |]
-    failsWith
-      [ DuplicateTypeDeclaration (Id "edge") (fromList [(NodeId 1, [U32, U32]), (NodeId 3, [U32, U32])])
-      , DuplicateTypeDeclaration (Id "path") (fromList [(NodeId 2, [U32, U32]), (NodeId 4, [U32, U32])])] [text|
-      @def edge(u32, u32).
-      @def path(u32, u32).
-      @def edge(u32, u32).
-      @def path(u32, u32).
-      |]
-
-  it "checks for unknown top level atoms" $ do
-    failsWith [UnknownAtom (NodeId 1) $ Id "edge"] [text|
-      edge(1, 2).
-      |]
-    failsWith [UnknownAtom (NodeId 1) $ Id "edge", UnknownAtom (NodeId 4) $ Id "path"] [text|
-      edge(1, 2).
-      path(3, 4).
-      |]
-
-  it "checks for unknown atoms in rule heads" $ do
-    failsWith [UnknownAtom (NodeId 2) $ Id "path"] [text|
-      @def edge(u32, u32).
-
-      path(x, y) :-
-        edge(x, y).
-      |]
-
-  it "checks for unknown atoms in rule bodies" $ do
-    failsWith [UnknownAtom (NodeId 5) $ Id "edge"] [text|
-      @def path(u32, u32).
-
-      path(x, y) :-
-        edge(x, y).
-      |]
-
-  it "checks for unknown atoms in mix of everything" $ do
-    failsWith
-      [ UnknownAtom (NodeId 1) $ Id "top_level_atom"
-      , UnknownAtom (NodeId 6) $ Id "edge"
-      , UnknownAtom (NodeId 3) $ Id "path"
-      ] [text|
-      top_level_atom(1).
-
-      path(x, y) :-
-        edge(x, y).
-      |]
-
   it "checks for type mismatch in top level atoms" $ do
-    failsWith
-      [ TypeMismatch (NodeId {unNodeId = 5}) U32 Str (WhileChecking (NodeId {unNodeId = 3}) :| [WhileChecking (NodeId {unNodeId = 5})])
-      , TypeMismatch (NodeId {unNodeId = 7}) Str U32 (WhileChecking (NodeId {unNodeId = 6}) :| [WhileChecking (NodeId {unNodeId = 7})])
-      , TypeMismatch (NodeId {unNodeId = 11}) U32 Str (WhileChecking (NodeId {unNodeId = 9}) :| [WhileChecking (NodeId {unNodeId = 11})])
-      , TypeMismatch (NodeId {unNodeId = 10}) Str U32 (WhileChecking (NodeId {unNodeId = 9}) :| [WhileChecking (NodeId {unNodeId = 10})])
-      , TypeMismatch (NodeId {unNodeId = 14}) U32 Str (WhileChecking (NodeId {unNodeId = 12}) :| [WhileChecking (NodeId {unNodeId = 14})])
-      , TypeMismatch (NodeId {unNodeId = 13}) U32 Str (WhileChecking (NodeId {unNodeId = 12}) :| [WhileChecking (NodeId {unNodeId = 13})])
-      ]
-      [text|
-      @def fact1(u32, string).
-      @def fact2(string, string).
-      fact1(1, 2).
-      fact1("abc", "def").
-      fact1("abc", 2).
-      fact2(1, 2).
-      |]
     typeChecks [text|
       @def fact1(u32, string).
       @def fact2(string, string).
@@ -121,18 +38,6 @@ spec = describe "typesystem" $ parallel $ do
       |]
 
   it "checks for type mismatch in rule heads" $ do
-    failsWith
-      [ TypeMismatch (NodeId {unNodeId = 5}) Str U32 (WhileChecking (NodeId {unNodeId = 3}) :| [WhileChecking (NodeId {unNodeId = 5})])
-      , TypeMismatch (NodeId {unNodeId = 10}) Str U32 (WhileChecking (NodeId {unNodeId = 9}) :| [WhileChecking (NodeId {unNodeId = 10})])
-      , TypeMismatch (NodeId {unNodeId = 17}) Str U32 (WhileChecking (NodeId {unNodeId = 15}) :| [WhileChecking (NodeId {unNodeId = 17})])
-      , TypeMismatch (NodeId {unNodeId = 16}) Str U32 (WhileChecking (NodeId {unNodeId = 15}) :| [WhileChecking (NodeId {unNodeId = 16})])
-      ] [text|
-      @def edge(u32, u32).
-      @def reachable(u32, u32).
-      reachable(x, "abc") :- edge(x, x).
-      reachable("abc", x) :- edge(x, x).
-      reachable("abc", "abc") :- edge(x, x).
-      |]
     typeChecks [text|
       @def edge(u32, u32).
       @def reachable(u32, u32).
@@ -141,19 +46,6 @@ spec = describe "typesystem" $ parallel $ do
       |]
 
   it "checks for type mismatch in rule bodies" $ do
-    failsWith
-      [ TypeMismatch (NodeId {unNodeId = 13}) Str U32 (WhileChecking (NodeId {unNodeId = 3}) :| [WhileChecking (NodeId {unNodeId = 13})])
-      , TypeMismatch (NodeId {unNodeId = 12}) Str U32 (WhileChecking (NodeId {unNodeId = 3}) :| [WhileChecking (NodeId {unNodeId = 12})])
-      , TypeMismatch (NodeId {unNodeId = 10}) Str U32 (WhileChecking (NodeId {unNodeId = 3}) :| [WhileChecking (NodeId {unNodeId = 10})])
-      , TypeMismatch (NodeId {unNodeId = 6}) Str U32 (WhileChecking (NodeId {unNodeId = 3}) :| [WhileChecking (NodeId {unNodeId = 6})])
-      ] [text|
-      @def fact1(u32, u32).
-      @def fact2(u32).
-      fact2(123) :-
-        fact1("abc", 123),
-        fact1(456, "def"),
-        fact1("abc", "def").
-      |]
     typeChecks [text|
       @def fact1(u32, u32).
       @def fact2(u32).
@@ -161,25 +53,7 @@ spec = describe "typesystem" $ parallel $ do
         fact1(123, 456).
       |]
 
-  it "checks for type mismatch of variables in rule heads" $ do
-    failsWith
-      [ TypeMismatch (NodeId {unNodeId = 5}) U32 Str (WhileChecking (NodeId 3) :| [WhileChecking (NodeId 5)])
-      ] [text|
-      @def fact1(u32).
-      @def fact2(u32, string).
-      fact2(x, x) :-
-        fact1(123).
-      |]
-
   it "checks for type mismatch of variables in rule bodies" $ do
-    failsWith
-      [ TypeMismatch (NodeId {unNodeId = 7}) U32 Str (WhileChecking (NodeId 3) :| [WhileChecking (NodeId 7)])
-      ] [text|
-      @def fact1(u32).
-      @def fact2(u32, string).
-      fact1(123) :-
-        fact2(x, x).
-      |]
     typeChecks [text|
       @def fact1(u32).
       @def fact2(u32, u32).
@@ -188,14 +62,6 @@ spec = describe "typesystem" $ parallel $ do
       |]
 
   it "checks for type mismatch of variables in entire rule" $ do
-    failsWith
-      [ TypeMismatch (NodeId {unNodeId = 7}) Str U32 (WhileChecking (NodeId 3) :| [WhileChecking (NodeId 7)])
-      ] [text|
-      @def fact1(u32).
-      @def fact2(u32, string).
-      fact2(123, x) :-
-        fact1(x).
-      |]
     typeChecks [text|
       @def fact1(u32).
       @def fact2(u32, string).
