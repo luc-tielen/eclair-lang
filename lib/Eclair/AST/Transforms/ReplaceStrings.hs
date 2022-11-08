283515a385dd8ab6ab2d9902cc3061a778dc9a7b
Luc Tielen <luc.tielen@gmail.com>
2022-11-08 07:56:06 +0100

    Store relation names in symbol table

--- lib/Eclair/AST/Transforms/ReplaceStrings.hs
+++ lib/Eclair/AST/Transforms/ReplaceStrings.hs
@@ -5,6 +5,7 @@ module Eclair.AST.Transforms.ReplaceStrings
 
 import qualified Data.Map as Map
 import Eclair.AST.IR
+import Eclair.Id
 import Eclair.Transform
 
 
@@ -18,6 +19,11 @@ transform =
   where
     rewrite :: RewriteRuleT (StateT StringMap) AST
     rewrite = \case
+      DeclareTypeF nodeId name tys -> do
+        -- By putting the relation names in the symbol table, we can easily
+        -- create a unique integer constant for the fact type mapping.
+        _ <- replaceString (unId name)
+        pure $ DeclareType nodeId name tys
       LitF nodeId (LString s) ->
         Lit nodeId . LNumber <$> replaceString s
       astf ->
