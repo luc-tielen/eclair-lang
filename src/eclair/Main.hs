8741beee4a7a831b19fb9bfaea5fc2a55d5efbd3
Luc Tielen <luc.tielen@gmail.com>
2022-12-08 11:18:39 +0100

    Allow running LSP via Eclair itself

--- src/eclair/Main.hs
+++ src/eclair/Main.hs
@@ -5,6 +5,9 @@ import Eclair
 import GHC.IO.Encoding
 import System.Directory
 import qualified Data.Text.IO as TIO
+import System.Process
+import Control.Exception
+import System.IO.Error
 
 
 tryReadFile :: FilePath -> IO (Maybe Text)
@@ -29,7 +32,14 @@ main = do
           params = Parameters (cpuTarget cfg) tryReadFile
       whenLeftM_ (fn params file) $ \errs -> do
         let errActions =
-              errs
-                & map handleErrorsCLI
-                & intersperse (TIO.hPutStr stderr "\n")
+              errs & map handleErrorsCLI
+                   & intersperse (TIO.hPutStr stderr "\n")
         sequence_ errActions
+
+    LSP -> do
+      findExecutable "eclair-lsp-server" >>= \case
+        Nothing -> do
+          putTextLn "Could not find 'eclair-lsp-server'. Make sure it is in your PATH."
+          exitFailure
+        Just lspExe -> do
+          callProcess lspExe [] `catch` (\(_ :: IOError) -> exitFailure)
